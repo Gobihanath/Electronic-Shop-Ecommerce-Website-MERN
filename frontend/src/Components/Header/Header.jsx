6d1bd2f5dc4c1a6f515d4076cf623673e837755c
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-05-20 10:45:55 +0530

    My Second Commit

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
     <div className='header-contents'>
     <h2>Unleashing the Power of Technology for You..!</h2>
-    <p>Welcome to Tech Haven, your ultimate online destination for the latest electronic devices. Explore our wide selection of cutting-edge gadgets designed to enhance your digital lifestyle. Enjoy seamless shopping with expert guidance and top-notch customer support, ensuring you find the perfect tech solutions for your needs.</p>
+    <p>Welcome to Tech Haven, your ultimate online destination for the latest electronic devices. Explore our wide selection of cutting-edge gadgets designed to enhance your digital lifestyle. </p>
     <button>View Menu</button>
     </div>
      
