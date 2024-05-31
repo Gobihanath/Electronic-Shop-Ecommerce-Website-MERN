5686661f7ea7d1f7ace872283c31264c350b66d8
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-05-31 09:10:52 +0530

    MyOrders page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Footer from './Components/Footer/Footer';
 import { useState } from 'react';
 import LoginPopup from './Components/LoginPopup/LoginPopup';
 import Verify from './Pages/Verify/Verify';
+import MyOrders from './Pages/MyOrders/MyOrders';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
         <Route path='/cart' element= {<Cart/>} />
         <Route path='/order' element= {<PlaceOrder/>} />
         <Route path='/verify' element= {<Verify/>}/>
+        <Route path='/myorders' element= {<MyOrders/>}/>
       </Routes>
     </div>
     <Footer/>
--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../Context/StoreContext'
 import axios from "axios";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 
 
@@ -59,6 +62,18 @@ const PlaceOrder = () => {
   //    console.log(data);
   // },[data])
 
+  const navigate = useNavigate();
+  
+
+  useEffect(()=>{
+    if(!token){
+      navigate("/cart")
+    }
+    else if(getTotalCartAmount()===0){
+      navigate("/cart")
+    }
+
+  },[token])
 
 
   return (
