08237fadc0d12a74e7ed888b8638872d47aff36f
Gobihanath Balasubramaniam <126072422+Gobihanath@users.noreply.github.com>
2024-05-31 14:31:15 +0530

    Completed! Frontend,Backend, Admin Panel

--- frontend/src/Components/AppDownload/AppDownload.jsx
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -6,7 +6,7 @@ const AppDownload = () => {
   return (
     <div className='app-download-container'>
     <div className='app-download' id='app-download'>
-       <p>Enhance your shopping experience with our mobile app. Download now for exclusive deals, faster checkout, and personalized recommendations! <br /> <br/> SparklinG</p>
+       <p>Enhance your shopping experience with our mobile app. Download now for exclusive deals, faster checkout, and personalized recommendations!<hr/> SparklinG</p>
        <div className='app-download-platforms'>
         <img src={assets.play_store} alt=''/>
         <img src={assets.app_store} alt=''/>
